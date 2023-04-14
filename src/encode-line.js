5d5aafff99cbbc093bc295d1176640a1cee58012
Nick GS <gsidnick@gmail.com>
2023-04-14 11:14:37 +0300

    feat: implement (ST) Encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (str === '') return '';
+  let result = [];
+  let groups = [];
+  let buffer = [];
+
+  [...str].forEach((char, index) => {
+    if (char === str[index + 1]) {
+      buffer.push(char);
+    } else {
+      buffer.push(char);
+      groups.push(buffer);
+      buffer = [];
+    }
+  });
+  groups.forEach((group) => {
+    if (group.length > 1) {
+      result.push(group.length);
+      result.push(group[0]);
+    } else {
+      result.push(group[0]);
+    }
+  })
+  return result.join('');
 }
 
 module.exports = {
